56bbaad976c115704bfc44205b6979048d80ae8c
Yanto <yanto.santoso1@gmail.com>
2026-06-01 16:19:35 +0700

    added addTodo system in JS

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 class Todo {
-    constructor(id, title, priority, completed = false) {
-        this.id = id;
-        this.title = title;
-        this.priority = priority;
-        this.completed = completed;
-    }
+  constructor(id, title, priority, completed = false) {
+    this.id = id;
+    this.title = title;
+    this.priority = priority;
+    this.completed = completed;
+  }
+}
+
+class TodoList {
+  constructor() {
+    this.todos = [];
+  }
+
+  addTodo(title, priority) {
+    const newTodo = new Todo(Date.now(), title, priority);
+    this.todos.push(newTodo);
+  }
 }
