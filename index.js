a10aa22bbbaa58eec0034d186fc4afd8dc582cf8
Yanto <yanto.santoso1@gmail.com>
2026-06-01 21:42:39 +0700

    added todo render and fetching 10 todos from an API placeholder

--- index.js
+++ index.js
@@ -31,3 +31,42 @@ class TodoList {
     todo.title = newTitle;
   }
 }
+
+const inputTodo = document.getElementById('inputTodo');
+const inputPriority = document.getElementById('inputPriority');
+const btnAdd = document.getElementById('btnAdd');
+const todoList = document.getElementById('todoList');
+
+const myList = new TodoList();
+
+function renderTodo() {
+  todoList.innerHTML = myList.todos
+    .map((todo) => {
+      return `
+      <li class="${todo.priority} ${todo.completed ? 'completed' : ''}">
+        <span>${todo.title}</span>
+        <div>
+          <button onclick="handleComplete(${todo.id})">Done</button>
+          <button onclick="handleEdit(${todo.id})">Edit</button>
+          <button onclick="handleDelete(${todo.id})">Delete</button>
+        </div>
+      </li>
+    `;
+    })
+    .join();
+}
+
+async function fetchTodos() {
+  try {
+    const response = await fetch(
+      'https://jsonplaceholder.typicode.com/todos?_limit=10'
+    );
+    const data = await response.json();
+    data.forEach((todo) => {
+      myList.addTodo(todo.title, 'low');
+    });
+    renderTodo();
+  } catch (error) {
+    todoList.innerHTML = `<p> Uh oh! Something went wrong. Error: ${error} </p>`;
+  }
+}
